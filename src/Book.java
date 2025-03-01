4332fd50edf267c211ebdd74e0479b7ced2257fa
Admin <ekaterina28041998@gmail.com>
2025-03-01 22:36:19 +0300

    Методы объектов. Ветка Домашней работы

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private int publishDate;
@@ -24,5 +26,27 @@ public class Book {
     public void setPublishDate(int publishDate) {
         this.publishDate = publishDate;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", publishDate=" + publishDate +
+                ", author=" + author +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishDate == book.publishDate && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publishDate, author);
+    }
 }
+
 
